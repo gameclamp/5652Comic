3a8c7319b533b0877006039992bc88ca24573049
9尾雪狐 <gameclamp@gmail.com>
2014-08-08 19:12:46 +0800

    Update 5652_comic.user.js

--- 5652_comic.user.js
+++ 5652_comic.user.js
@@ -5,7 +5,7 @@
 // @include      http://mh.5652.com/mh/*/index.shtml
 // @description  5652多图脚本。
 // @icon         http://mh.5652.com/favicon.ico
-// @version      1.2
+// @version      1.3
 // @updateURL    https://github.com/gameclamp/5652-Comic/raw/master/5652_comic.user.js
 // @grant        none
 // ==/UserScript==
